e22e7a37e4217688b3a2f863d9df4f71bcd0815a
jigaji <ts.saglara@gmail.com>
2022-08-08 21:37:34 +0300

    first commit

--- src/js/__tests__/app.test.js
+++
@@ -1,9 +0,0 @@
-import reandomSquare from '../app'
-
-test('delete goblin class', () => {
-    const squares = document.querySelectorAll('.square');
-    squares.forEach(square => {
-        square.classList.remove('goblin');
-        expected(square.className).toBe('square')
-    })
-});
